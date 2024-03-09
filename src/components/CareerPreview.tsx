2802ac302ad59dfd159c861de183c5a5a20d23e5
pirotyyy <hiroki1423@docomo.ne.jp>
2024-03-09 23:38:54 +0900

    refactor: remove div tag

--- src/components/CareerPreview.tsx
+++ src/components/CareerPreview.tsx
@@ -10,24 +10,22 @@ const CareerPreview = (props: Props) => {
   const { user } = props;
 
   return (
-    <div className="h-screen">
-      <div className="h-full outline outline-2 outline-gray-200 rounded">
-        <div className="flex h-full justify-center items-center">
-          {
-            // NOTE: ナジャさんの作成したコンポーネント置く。
-            // 仮にHoverしたユーザーの情報を表示させている。
-          }
-          {user ? (
-            <div className="flex-row items-center">
-              <UserIcon iconImageHash={user.iconImageHash} size={128} />
-              <div className="text-center">
-                <span>{user.firstname + " " + user.lastname}</span>
-              </div>
+    <div className="h-full outline outline-2 outline-gray-200 rounded">
+      <div className="flex h-full justify-center items-center">
+        {
+          // NOTE: ナジャさんの作成したコンポーネント置く。
+          // 仮にHoverしたユーザーの情報を表示させている。
+        }
+        {user ? (
+          <div className="flex-row items-center">
+            <UserIcon iconImageHash={user.iconImageHash} size={128} />
+            <div className="text-center">
+              <span>{user.firstname + " " + user.lastname}</span>
             </div>
-          ) : (
-            <p>any user not hovered</p>
-          )}
-        </div>
+          </div>
+        ) : (
+          <p>any user not hovered</p>
+        )}
       </div>
     </div>
   );
