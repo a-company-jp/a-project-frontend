109fd7744ce84f8f91eb719a9dc68b35d7ffce06
pirotyyy <hiroki1423@docomo.ne.jp>
2024-03-11 03:10:29 +0900

    feat: 動作確認用のページ作成

---
+++ src/app/setting/page.tsx
@@ -0,0 +1,13 @@
+import Sidebar from "@/components/Sidebar";
+import React from "react";
+
+const Setting = () => {
+  return (
+    <div className="h-screen w-screen flex">
+      <Sidebar />
+      <div className="w-full grid grid-cols-5">something</div>
+    </div>
+  );
+};
+
+export default Setting;
