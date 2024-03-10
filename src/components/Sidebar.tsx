0ae42f83ce3f3ced5f5d113f6ed48a39e9459658
pirotyyy <hiroki1423@docomo.ne.jp>
2024-03-11 03:09:38 +0900

    feat: SidebarIconを使ってSidebarを作成

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,8 +1,35 @@
 import React from "react";
+import UserIcon from "./UserIcon";
+import SidebarIcon from "./SidebarIcon";
 
 const Sidebar = () => {
   return (
-    <div className="h-screen outline outline-2 outline-gray-200 ">Sidebar</div>
+    <div className="w-24 h-screen outline outline-2 items-center flex flex-col justify-between outline-gray-200 ">
+      <div className="my-4 flex flex-col gap-y-4">
+        <SidebarIcon
+          icon={
+            <span className="material-symbols-outlined material-icons text-3xl">
+              home
+            </span>
+          }
+          path="/main"
+        />
+        <SidebarIcon
+          icon={
+            <span className="material-symbols-outlined material-icons text-3xl">
+              settings
+            </span>
+          }
+          path="/setting"
+        />
+      </div>
+      <div className="my-4">
+        <UserIcon
+          iconImageHash="https://storage.googleapis.com/dev-open-hacku-bucket/dev-person-images/person6.jpg"
+          size={48}
+        />
+      </div>
+    </div>
   );
 };
 
