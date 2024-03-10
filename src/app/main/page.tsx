c43f3641c0bb75ea07f7dedab83925898982dcde
pirotyyy <hiroki1423@docomo.ne.jp>
2024-03-10 22:19:38 +0900

    fix: page.tsxのdiv減らす

--- src/app/main/page.tsx
+++ src/app/main/page.tsx
@@ -11,31 +11,21 @@ const Main = () => {
   const [hoveredUser, setHoveredUser] = useState<UserData | null>(null);
 
   return (
-    <div className="h-screen w-screen">
-      <div className="grid grid-cols-10">
-        <div className="col-span-1">
-          <Sidebar />
-        </div>
-        <div className="col-span-9">
-          <div className="grid grid-cols-5">
-            <div className="col-span-3">
-              <div className="p-4 h-screen overflow-scroll hidden-scrollbar">
-                <div className="h-max">
-                  <UserSearchForm />
-                </div>
-                <div className="py-4 items-center">
-                  <UserList
-                    setHoveredUser={setHoveredUser}
-                    hoveredUser={hoveredUser}
-                  />
-                </div>
-              </div>
-            </div>
-            <div className="col-span-2">
-              <div className="pt-4 pr-4 pb-4 h-screen">
-                <CareerPreview user={hoveredUser} />
-              </div>
-            </div>
+    <div className="h-screen w-screen grid grid-cols-10">
+      <div className="col-span-1">
+        <Sidebar />
+      </div>
+      <div className="col-span-9">
+        <div className="grid grid-cols-5">
+          <div className="col-span-3 p-4 h-screen overflow-scroll hidden-scrollbar">
+            <UserSearchForm />
+            <UserList
+              setHoveredUser={setHoveredUser}
+              hoveredUser={hoveredUser}
+            />
+          </div>
+          <div className="col-span-2">
+            <CareerPreview user={hoveredUser} />
           </div>
         </div>
       </div>
