19ee775b1f73adc612e3ff7a4172a29ba8684a30
Ishigami100 <yamahaya5151@icloud.com>
2024-03-11 01:20:36 +0900

    feat login

---
+++ src/app/login/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import LoginButton from "@/components/LoginButton";
+
+const Main = () => {
+  return (
+    <div>
+      <LoginButton />
+    </div>
+  )
+};
+
+export default Main;
---
+++ src/components/LoginButton.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const LoginButton = () => {
+    //ログインボタン
+    return (
+        <form>
+          <button id="buttonSignin">Sign in</button>
+        </form>
+    );
+}
+
+export default LoginButton;
