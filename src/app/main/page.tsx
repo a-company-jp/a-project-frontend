6b6ab34f3af92e0a005d04aa7e5d886c5bf34bd8
daiki0381 <daiki03810218@gmail.com>
2024-03-12 03:13:01 +0900

    🐛 /login以外のページはログインを必須にする

--- src/app/main/page.tsx
+++ src/app/main/page.tsx
@@ -1,16 +1,34 @@
 "use client";
 
-import CareerPreview from "@/components/organisms/CareerPreview"; 
+import CareerPreview from "@/components/organisms/CareerPreview";
 import Sidebar from "@/components/organisms/Sidebar";
-import UserList from "@/components/organisms/UserList"; 
+import UserList from "@/components/organisms/UserList";
 import UserSearchForm from "@/components/molecules/UserSearchForm";
 import { useState } from "react";
 import { UserData } from "../../../proto/typescript/pb_out/main";
+import { useRouter } from "next/navigation";
+import { auth } from "@/lib/firebase/client";
+import { useAuthState } from "react-firebase-hooks/auth";
 
 const Main = () => {
+  const router = useRouter();
   const [hoveredUser, setHoveredUser] = useState<UserData | null>(null);
+  const [user, isLoading] = useAuthState(auth);
+  const redirectLogin = () => {
+    router.push("/login");
+  };
 
-  return (
+  if (isLoading)
+    return (
+      <div
+        className="flex justify-center items-center h-screen"
+        aria-label="Loading..."
+      >
+        <div className="animate-spin h-10 w-10 border-4 border-gray-500 rounded-full border-t-transparent"></div>
+      </div>
+    );
+
+  return user ? (
     <div className="h-screen w-screen flex">
       <Sidebar />
       <div className="w-full grid grid-cols-5">
@@ -23,6 +41,8 @@ const Main = () => {
         </div>
       </div>
     </div>
+  ) : (
+    redirectLogin()
   );
 };
 
--- src/app/setting/page.tsx
+++ src/app/setting/page.tsx
@@ -1,13 +1,34 @@
-'use client'
+"use client";
 
-import Sidebar from "@/components/organisms/Sidebar"; 
+import Sidebar from "@/components/organisms/Sidebar";
+import { useRouter } from "next/navigation";
+import { auth } from "@/lib/firebase/client";
+import { useAuthState } from "react-firebase-hooks/auth";
 
 const Setting = () => {
-  return (
+  const router = useRouter();
+  const [user, isLoading] = useAuthState(auth);
+  const redirectLogin = () => {
+    router.push("/login");
+  };
+
+  if (isLoading)
+    return (
+      <div
+        className="flex justify-center items-center h-screen"
+        aria-label="Loading..."
+      >
+        <div className="animate-spin h-10 w-10 border-4 border-gray-500 rounded-full border-t-transparent"></div>
+      </div>
+    );
+
+  return user ? (
     <div className="h-screen w-screen flex">
       <Sidebar />
       <div className="w-full grid grid-cols-5">プロフィール設定</div>
     </div>
+  ) : (
+    redirectLogin()
   );
 };
 
