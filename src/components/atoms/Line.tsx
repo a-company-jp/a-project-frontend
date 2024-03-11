69c72b04c6517de5c9303f0fb8119ed8602d4762
Najah7 <najahmatsuo7@gmail.com>
2024-03-11 11:09:26 +0900

    [Fix] Line component

--- src/components/atoms/Line.tsx
+++ src/components/atoms/Line.tsx
@@ -2,17 +2,10 @@ import React from 'react'
 
 type Props = {}
 
-const Line = (props: Props) => {
-    const BoxStyle = 'relative md:w-48 w-24 md:h-24 h-12'
-    const LineStyle = 'move-line-right absolute left-0 top-0 bottom-0 w-1 bg-gray-300'
-
-    return (
-        <>
-            <div className={BoxStyle}>
-                <div className={LineStyle}></div>
-            </div>
-        </>
-    )
+const styles = {
+    boxing: 'w-1/2 md:h-24 h-12 border-l-4'
 }
+
+const Line = (props: Props) => (<div className={styles.boxing}></div>)
 
 export default Line
