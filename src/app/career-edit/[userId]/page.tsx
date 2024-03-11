55556db626832dd1774750209e27178f0d8146ab
hirokiwa <hirokifsys@gmail.com>
2024-03-11 23:05:41 +0900

    キャリア編集ページを作成

---
+++ src/app/career-edit/[userId]/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const careerEdit = () => {
+  return (
+    <div className="h-screen w-screen flex">
+      <div className="w-full grid grid-cols-5">something</div>
+    </div>
+  );
+};
+
+export default careerEdit;
