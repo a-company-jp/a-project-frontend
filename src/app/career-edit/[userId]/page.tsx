6583f368e4249174b59bdc5b4f799d5a740d7ff8
hirokiwa <hirokifsys@gmail.com>
2024-03-11 23:28:52 +0900

    Googleカレンダー風UIを表示

--- src/app/career-edit/[userId]/page.tsx
+++ src/app/career-edit/[userId]/page.tsx
@@ -1,9 +1,10 @@
+import EditingCareerCalendar from "@/components/EditingCareerCarendar";
 import React from "react";
 
 const careerEdit = () => {
   return (
-    <div className="h-screen w-screen flex">
-      <div className="w-full grid grid-cols-5">something</div>
+    <div className="p-5">
+      <EditingCareerCalendar />
     </div>
   );
 };
