a12607ca631fa3b78582a8c44755ef413d179c24
hirokiwa <hirokifsys@gmail.com>
2024-03-15 06:59:30 +0900

    項目間の隙間を開ける

--- src/components/organisms/EditMilestoneForm.tsx
+++ src/components/organisms/EditMilestoneForm.tsx
@@ -62,75 +62,79 @@ const EditMilestoneForm = ({
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col">
-      <label className="flex gap-4">
-        <p>期間</p>
-        <strong className={style.inputRequired}>*必須</strong>
-      </label>
-      <div className="flex items-center gap-3">
-        <input
-          type="month"
-          id="beginMonth"
-          title="開始時期"
-          {...register("beginMonth", {
-            required: "期間の入力は必須です。",
-            validate: handleValidateBeginMonth,
-          })}
-          defaultValue={`${YYYYMMDDToYYYYMM(lifeEvent.beginDate)}`}
-          min={`${START_YEAR}-01`}
-          max={`${START_YEAR + FULL_YEAR}-03`}
-          className={`${style.input.default} ${
-            errors.beginMonth && style.input.error
-          }`}
-          required
-        />
-        <p>〜</p>
-        <input
-          type="month"
-          id="endMonth"
-          title="終了時期"
-          {...register("endMonth", {
-            required: "期間の入力は必須です。",
-            validate: handleValidateBeginMonth,
-          })}
-          defaultValue={`${YYYYMMDDToYYYYMM(lifeEvent.finishDate)}`}
-          min={`${START_YEAR}-01`}
-          max={`${START_YEAR + FULL_YEAR}-03`}
-          className={`${style.input.default} ${
-            errors.endMonth && style.input.error
-          }`}
-          required
-        />
+    <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-4">
+      <div className="flex flex-col">
+        <label className="flex gap-4">
+          <p>期間</p>
+          <strong className={style.inputRequired}>*必須</strong>
+        </label>
+        <div className="flex items-center gap-3">
+          <input
+            type="month"
+            id="beginMonth"
+            title="開始時期"
+            {...register("beginMonth", {
+              required: "期間の入力は必須です。",
+              validate: handleValidateBeginMonth,
+            })}
+            defaultValue={`${YYYYMMDDToYYYYMM(lifeEvent.beginDate)}`}
+            min={`${START_YEAR}-01`}
+            max={`${START_YEAR + FULL_YEAR}-03`}
+            className={`${style.input.default} ${
+              errors.beginMonth && style.input.error
+            }`}
+            required
+          />
+          <p>〜</p>
+          <input
+            type="month"
+            id="endMonth"
+            title="終了時期"
+            {...register("endMonth", {
+              required: "期間の入力は必須です。",
+              validate: handleValidateBeginMonth,
+            })}
+            defaultValue={`${YYYYMMDDToYYYYMM(lifeEvent.finishDate)}`}
+            min={`${START_YEAR}-01`}
+            max={`${START_YEAR + FULL_YEAR}-03`}
+            className={`${style.input.default} ${
+              errors.endMonth && style.input.error
+            }`}
+            required
+          />
+        </div>
+        <strong className={style.inputErrorMessage}>
+          {errors.beginMonth?.message}
+        </strong>
       </div>
-      <strong className={style.inputErrorMessage}>
-        {errors.beginMonth?.message}
-      </strong>
-
-      <label htmlFor="milestoneTitle" className="flex gap-4">
-        <p>タイトル</p>
-        <strong className={style.inputRequired}>*必須</strong>
-      </label>
-      <input
-        id={"milestoneTitle"}
-        defaultValue={lifeEvent.title}
-        placeholder="例) マネージャーとしてチームをリードする"
-        {...register("title", { required: "タイトルの入力は必須です。" })}
-        className={`${style.input.default} ${
-          errors.title && style.input.error
-        }`}
-      />
-      <strong className={style.inputErrorMessage}>
-        {errors.title?.message}
-      </strong>
-
-      <label htmlFor="milestoneContent">内容</label>
-      <textarea
-        id={"milestoneContent"}
-        {...register("content", { required: true })}
-        className={`${style.input.default} h-60`}
-      >
-        {lifeEvent.content}
-      </textarea>
+      <div className="flex flex-col">
+        <label htmlFor="milestoneTitle" className="flex gap-4">
+          <p>タイトル</p>
+          <strong className={style.inputRequired}>*必須</strong>
+        </label>
+        <input
+          id={"milestoneTitle"}
+          defaultValue={lifeEvent.title}
+          placeholder="例) マネージャーとしてチームをリードする"
+          {...register("title", { required: "タイトルの入力は必須です。" })}
+          className={`${style.input.default} ${
+            errors.title && style.input.error
+          }`}
+        />
+        <strong className={style.inputErrorMessage}>
+          {errors.title?.message}
+        </strong>
+      </div>
+      <div className="flex flex-col">
+        <label htmlFor="milestoneContent">内容</label>
+        <textarea
+          id={"milestoneContent"}
+          {...register("content", { required: true })}
+          className={`${style.input.default} h-60`}
+        >
+          {lifeEvent.content}
+        </textarea>
+      </div>
       <div className="flex gap-4 justify-stretch px-8">
         <button
           onClick={closeModal}
