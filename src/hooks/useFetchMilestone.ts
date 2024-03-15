eacfad943b6723e06403f2fa72da9f2d5829a3c5
Najah7 <najahmatsuo7@gmail.com>
2024-03-16 05:16:54 +0900

    [Add] API callの関数をとりあえず実装。

--- src/hooks/useFetchMilestone.ts
+++ src/hooks/useFetchMilestone.ts
@@ -3,112 +3,56 @@ import axios from "axios";
 import {
   Milestone,
   MilestoneCreateRequest,
+  MilestoneCreateResponse,
 } from "../../proto/typescript/pb_out/main";
+
+import useApiPBClient from "@/hooks/useApiPBClient";
 
 const protobuf = require("protobufjs");
 
 const useFetchMilestone = () => {
-  const create = async (newMileStone: Milestone): Promise<Milestone | void> => {
-    // マイルストーンを新規作成する関数
-    // 新しいマイルストーンをサーバーに送信する (milestoneIdは空文字で送信)
-    // サーバー側で付与されたmilestoneID または milestone 丸ごとを返す
-    const token = localStorage.getItem("token");
+  const client = useApiPBClient();
 
-    const MilestoneCreateRequest = protobuf.roots.default.lookupType(
-      "main.MilestoneCreateRequest",
-    );
+  const create = (
+    newMilestoen: MilestoneCreateRequest,
+  ): Promise<MilestoneCreateResponse> => {
+    client.post("/milestone", newMilestoen).then((resp) => {
+      if (resp.unauthorized) {
+        throw new Error("unauthorized");
+      }
+      if (resp.error) {
+        throw new Error(resp.error);
+      }
+      return MilestoneCreateResponse.fromBinary(resp.data);
+    });
+  };
 
-    const payload = { milestone: newMileStone } as MilestoneCreateRequest;
-
-    const errMsg = MilestoneCreateRequest.verify(payload);
-    if (errMsg) throw Error(`MilestoneCreateRequest: ${errMsg}`);
-
-    const milestone = MilestoneCreateRequest.create(payload);
-    const buffer = MilestoneCreateRequest.encode(milestone).finish();
-
-    await axios
-      .post(`${process.env.BACKEND_DOMAIN}/api/v1/milestone/create`, buffer, {
-        headers: {
-          Authorization: `token ${token}`,
-        },
-      })
-      .then((response) => {
-        const MilestoneCreateResponse = protobuf.roots.default.lookupType(
-          "main.MilestoneCreateResponse",
-        );
-        const res = MilestoneCreateResponse.decode(
-          new Uint8Array(response.data),
-        );
-        return MilestoneCreateResponse.toObject(res);
-      })
-      .catch((error) => {
-        console.log(error);
+  const update = async (newMilestoen: MilestoneCreateRequest) => {
+    return await client
+      .put(`/milestone/${newMilestoen.milestone?.milestoneId}`, newMilestoen)
+      .then((resp) => {
+        if (resp.unauthorized) {
+          throw new Error("unauthorized");
+        }
+        if (resp.error) {
+          throw new Error(resp.error);
+        }
+        return MilestoneUpdateResponse.fromBinary(resp.data);
       });
   };
 
-  const update = async (newMileStone: Milestone): Promise<Milestone | void> => {
-    // マイルストーンを更新する関数
-    // 更新したいマイルストーンをサーバーに送信
-    // 更新されたマイルストーン または ステータス または void を返す
-    const token = localStorage.getItem("token");
-
-    const MilestoneUpdateRequest = protobuf.roots.default.lookupType(
-      "main.MilestoneUpdateRequest",
-    );
-    const payload = { milestone: newMileStone };
-    const errMsg = MilestoneUpdateRequest.verify(payload);
-    if (errMsg) throw Error(`MilestoneUpdateRequest: ${errMsg}`);
-
-    const milestone = MilestoneUpdateRequest.create(payload);
-    const buffer = MilestoneUpdateRequest.encode(milestone).finish();
-    await axios
-      .post(`${process.env.BACKEND_DOMAIN}/api/v1/milestone/update`, buffer, {
-        headers: {
-          Authorization: `token ${token}`,
-        },
-      })
-      .then((response) => {
-        const MilestoneCreateResponse = protobuf.roots.default.lookupType(
-          "main.MilestoneCreateResponse",
-        );
-        const res = MilestoneCreateResponse.decode(
-          new Uint8Array(response.data),
-        );
-        return res;
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  };
-
-  // because delete is a reserved word, we use _delete
-  const _delete = async (milestoneId: string): Promise<string | void> => {
-    // マイルストーンを削除する関数
-    // 指定されたmilestoneIdのマイルストーンを削除する
-    // ステータス または void を返す
-    const token = localStorage.getItem("token");
-
-    await axios
-      .delete(
-        `${process.env.BACKEND_DOMAIN}/api/v1/milestone/delete${milestoneId}`,
-        {
-          headers: {
-            Authorization: `token ${token}`,
-          },
-        },
-      )
-      .then((response) => {
-        const MilestoneDeleteResponse = protobuf.roots.default.lookupType(
-          "main.MilestoneDeleteResponse",
-        );
-        const res = MilestoneDeleteResponse.decode(
-          new Uint8Array(response.data),
-        );
-        return res;
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+  const _delete = async (
+    milestoneId: string,
+  ): Promise<MilestoneDeleteResponse> => {
+    return await client.delete(`/milestone/${milestoneId}`).then((resp) => {
+      if (resp.unauthorized) {
+        throw new Error("unauthorized");
+      }
+      if (resp.error) {
+        throw new Error(resp.error);
+      }
+      return MilestoneDeleteResponse.fromBinary(resp.data);
+    });
   };
 
   return { create, update, _delete };
--- src/hooks/useFetchUser.ts
+++ src/hooks/useFetchUser.ts
@@ -1,12 +1,29 @@
 "use client";
 import axios from "axios";
-import { UserData, UserInfoResponse } from "../../proto/typescript/pb_out/main";
+import {
+  UserData,
+  UserInfoResponse,
+  UserInfosResponse,
+} from "../../proto/typescript/pb_out/main";
 import useApiPBClient from "@/hooks/useApiPBClient";
 
 const protobuf = require("protobufjs");
 
 const useFetchUser = () => {
   const client = useApiPBClient();
+
+  const me = async (): Promise<UserInfoResponse> => {
+    return await client.get(`/api/v1/user/info/me`).then((resp) => {
+      if (resp.unauthorized) {
+        throw new Error("unauthorized");
+      }
+      if (resp.error) {
+        throw new Error(resp.error);
+      }
+      return UserInfoResponse.fromBinary(resp.data);
+    });
+  };
+
   const get = async (userId: string): Promise<UserInfoResponse> => {
     return await client.get(`/api/v1/user/info/${userId}`).then((resp) => {
       if (resp.unauthorized) {
@@ -19,93 +36,45 @@ const useFetchUser = () => {
     });
   };
 
-  const list = async (): Promise<UserData[] | void> => {
-    const token = localStorage.getItem("token");
-
-    await axios
-      .get(
-        `${process.env.BACKEND_DOMAIN}/api/v1/user/info`, // TODO: rename to /users
-        {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        },
-      )
-      .then((response) => {
-        const UsersResponse =
-          protobuf.roots.default.lookupType("main.UsersResponse");
-        const res = UsersResponse.decode(new Uint8Array(response.data));
-        return UsersResponse.toObject(res);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+  const list = async (): Promise<UserInfosResponse> => {
+    return await client.get("/api/v1/user/info/").then((resp) => {
+      if (resp.unauthorized) {
+        throw new Error("unauthorized");
+      }
+      if (resp.error) {
+        throw new Error(resp.error);
+      }
+      return UserInfosResponse.fromBinary(resp.data);
+    });
   };
 
-  const create = async (newUser: UserData): Promise<UserData | void> => {
-    const token = localStorage.getItem("token");
-
-    const UserCreateRequest = protobuf.roots.default.lookupType(
-      "main.UserCreateRequest",
-    );
-    const payload = { user: newUser };
-    const errMsg = UserCreateRequest.verify(payload);
-    if (errMsg) throw Error(`UserCreateRequest: ${errMsg}`);
-
-    const user = UserCreateRequest.create(payload);
-    const buffer = UserCreateRequest.encode(user).finish();
-
-    await axios
-      .post(`${process.env.BACKEND_DOMAIN}/api/v1/user/create`, buffer, {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      })
-      .then((response) => {
-        const UserCreateResponse = protobuf.roots.default.lookupType(
-          "main.UserCreateResponse",
-        );
-        const res = UserCreateResponse.decode(new Uint8Array(response.data));
-        return res;
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+  const create = async (newUser: UserData): Promise<UserInfoResponse> => {
+    return await client.put(`/user/${newUser.userId}`, newUser).then((resp) => {
+      if (resp.unauthorized) {
+        throw new Error("unauthorized");
+      }
+      if (resp.error) {
+        throw new Error(resp.error);
+      }
+      return UserInfoResponse.fromBinary(resp.data);
+    });
   };
 
-  const update = async (newUser: UserData): Promise<UserData | void> => {
-    const token = localStorage.getItem("token");
+  //   const icon = async (userId: string, file: File) => {
+  //     return await client.put(
+  //         `/user/${userId}/icon`,
+  //         ).then((resp) => {
+  //         if (resp.unauthorized) {
+  //             throw new Error("unauthorized");
+  //           }
+  //           if (resp.error) {
+  //             throw new Error(resp.error);
+  //           }
+  //           return UserInfoResponse.fromBinary(resp.data)
+  //     })
+  //   }
 
-    const UserUpdateRequest = protobuf.roots.default.lookupType(
-      "main.UserUpdateRequest",
-    );
-    const payload = { user: newUser };
-    const errMsg = UserUpdateRequest.verify(payload);
-
-    if (errMsg) throw Error(`UserUpdateRequest: ${errMsg}`);
-
-    const user = UserUpdateRequest.create(payload);
-    const buffer = UserUpdateRequest.encode(user).finish();
-
-    await axios
-      .post(`${process.env.BACKEND_DOMAIN}/api/v1/user/update`, buffer, {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      })
-      .then((response) => {
-        const UserUpdateResponse = protobuf.roots.default.lookupType(
-          "main.UserUpdateResponse",
-        );
-        const res = UserUpdateResponse.decode(new Uint8Array(response.data));
-        return res;
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  };
-
-  return { get, list, create, update };
+  return { me, get, list, create };
 };
 
 export default useFetchUser;
