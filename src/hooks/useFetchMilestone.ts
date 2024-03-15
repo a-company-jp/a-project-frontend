45d32642e89fcd8baba0fbaf69a282c24700c104
Najah7 <najahmatsuo7@gmail.com>
2024-03-16 03:06:00 +0900

    [Format]

--- src/hooks/useFetchMilestone.ts
+++ src/hooks/useFetchMilestone.ts
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import axios from "axios";
 const protobuf = require("protobufjs");
 
@@ -6,15 +6,16 @@ import { Milestone } from "../../proto/typescript/pb_out/main";
 import mockMilestones from "@/sample-data/milestones.json";
 
 const useFetchMilestone = () => {
-  
-  const create = async (newMileStone: Milestone): Promise<Milestone|void> => {
+  const create = async (newMileStone: Milestone): Promise<Milestone | void> => {
     // マイルストーンを新規作成する関数
     // 新しいマイルストーンをサーバーに送信する (milestoneIdは空文字で送信)
     // サーバー側で付与されたmilestoneID または milestone 丸ごとを返す
     const token = localStorage.getItem("token");
 
-    const MilestoneCreateRequest = protobuf.roots.default.lookupType("main.MilestoneCreateRequest");
-    
+    const MilestoneCreateRequest = protobuf.roots.default.lookupType(
+      "main.MilestoneCreateRequest",
+    );
+
     const payload = { milestone: newMileStone };
 
     const errMsg = MilestoneCreateRequest.verify(payload);
@@ -23,57 +24,61 @@ const useFetchMilestone = () => {
     const milestone = MilestoneCreateRequest.create(payload);
     const buffer = MilestoneCreateRequest.encode(milestone).finish();
 
-    await axios.post(
-      `${process.env.BACKEND_DOMAIN}/api/v1/milestone/create`,
-      buffer,
-      {
-          headers: {
-              Authorization: `token ${token}`,
-          },
-      }
-    )
-    .then((response) => {
-      const MilestoneCreateResponse = protobuf.roots.default.lookupType("main.MilestoneCreateResponse");
-      const res = MilestoneCreateResponse.decode(new Uint8Array(response.data));
-      return MilestoneCreateResponse.toObject(res);
-    })
-    .catch((error) => {
-      console.log(error);
-    })
-  }
-  
+    await axios
+      .post(`${process.env.BACKEND_DOMAIN}/api/v1/milestone/create`, buffer, {
+        headers: {
+          Authorization: `token ${token}`,
+        },
+      })
+      .then((response) => {
+        const MilestoneCreateResponse = protobuf.roots.default.lookupType(
+          "main.MilestoneCreateResponse",
+        );
+        const res = MilestoneCreateResponse.decode(
+          new Uint8Array(response.data),
+        );
+        return MilestoneCreateResponse.toObject(res);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   const update = async (newMileStone: Milestone): Promise<Milestone | void> => {
     // マイルストーンを更新する関数
     // 更新したいマイルストーンをサーバーに送信
     // 更新されたマイルストーン または ステータス または void を返す
     const token = localStorage.getItem("token");
 
-    const MilestoneUpdateRequest = protobuf.roots.default.lookupType("main.MilestoneUpdateRequest");
+    const MilestoneUpdateRequest = protobuf.roots.default.lookupType(
+      "main.MilestoneUpdateRequest",
+    );
     const payload = { milestone: newMileStone };
     const errMsg = MilestoneUpdateRequest.verify(payload);
     if (errMsg) throw Error(`MilestoneUpdateRequest: ${errMsg}`);
 
     const milestone = MilestoneUpdateRequest.create(payload);
     const buffer = MilestoneUpdateRequest.encode(milestone).finish();
-    await axios.post(
-      `${process.env.BACKEND_DOMAIN}/api/v1/milestone/update`,
-      buffer,
-      {
-          headers: {
-              Authorization: `token ${token}`,
-          },
-      }
-    )
-    .then((response) => {
-      const MilestoneCreateResponse = protobuf.roots.default.lookupType("main.MilestoneCreateResponse");
-      const res = MilestoneCreateResponse.decode(new Uint8Array(response.data));
-      return res
-    })
-    .catch((error) => {
-      console.log(error);
-    })
-}
-  
+    await axios
+      .post(`${process.env.BACKEND_DOMAIN}/api/v1/milestone/update`, buffer, {
+        headers: {
+          Authorization: `token ${token}`,
+        },
+      })
+      .then((response) => {
+        const MilestoneCreateResponse = protobuf.roots.default.lookupType(
+          "main.MilestoneCreateResponse",
+        );
+        const res = MilestoneCreateResponse.decode(
+          new Uint8Array(response.data),
+        );
+        return res;
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   // because delete is a reserved word, we use _delete
   const _delete = async (milestoneId: string): Promise<string | void> => {
     // マイルストーンを削除する関数
@@ -81,26 +86,30 @@ const useFetchMilestone = () => {
     // ステータス または void を返す
     const token = localStorage.getItem("token");
 
-    await axios.delete(
-      `${process.env.BACKEND_DOMAIN}/api/v1/milestone/delete${milestoneId}`,
-      {
+    await axios
+      .delete(
+        `${process.env.BACKEND_DOMAIN}/api/v1/milestone/delete${milestoneId}`,
+        {
           headers: {
-              Authorization: `token ${token}`,
+            Authorization: `token ${token}`,
           },
-      }
-    )
-    .then((response) => {
-      const MilestoneDeleteResponse = protobuf.roots.default.lookupType("main.MilestoneDeleteResponse");
-      const res = MilestoneDeleteResponse.decode(new Uint8Array(response.data));
-      return res
-    })
-    .catch((error) => {
-      console.log(error);
-    })
+        },
+      )
+      .then((response) => {
+        const MilestoneDeleteResponse = protobuf.roots.default.lookupType(
+          "main.MilestoneDeleteResponse",
+        );
+        const res = MilestoneDeleteResponse.decode(
+          new Uint8Array(response.data),
+        );
+        return res;
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
-  }
-
-  return {create, update, _delete};
-}
+  return { create, update, _delete };
+};
 
 export default useFetchMilestone;
--- src/hooks/useImageUploder.ts
+++ src/hooks/useImageUploder.ts
@@ -1,39 +1,38 @@
 import axios from "axios";
 const protobuf = require("protobufjs");
 
-
 const useImageUploder = () => {
+  const upload = async (image: File): Promise<string | void> => {
+    const token = localStorage.getItem("token");
 
-    const upload = async (image: File): Promise<string | void> => {
-        const token = localStorage.getItem("token");
+    const ImageUploadRequest = protobuf.roots.default.lookupType(
+      "main.ImageUploadRequest",
+    );
+    const payload = { image: image };
+    const errMsg = ImageUploadRequest.verify(payload);
+    if (errMsg) throw Error(`ImageUploadRequest: ${errMsg}`);
 
-        const ImageUploadRequest = protobuf.roots.default.lookupType("main.ImageUploadRequest");
-        const payload = { image: image };
-        const errMsg = ImageUploadRequest.verify(payload);
-        if (errMsg) throw Error(`ImageUploadRequest: ${errMsg}`);
+    const imageUpload = ImageUploadRequest.create(payload);
+    const buffer = ImageUploadRequest.encode(imageUpload).finish();
+    await axios
+      .post(`${process.env.BACKEND_DOMAIN}/api/v1/image/upload`, buffer, {
+        headers: {
+          Authorization: `token ${token}`,
+        },
+      })
+      .then((response) => {
+        const ImageUploadResponse = protobuf.roots.default.lookupType(
+          "main.ImageUploadResponse",
+        );
+        const res = ImageUploadResponse.decode(new Uint8Array(response.data));
+        return res;
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
-        const imageUpload = ImageUploadRequest.create(payload);
-        const buffer = ImageUploadRequest.encode(imageUpload).finish();
-        await axios.post(
-            `${process.env.BACKEND_DOMAIN}/api/v1/image/upload`,
-            buffer,
-            {
-                headers: {
-                    Authorization: `token ${token}`,
-                },
-            }
-        )
-        .then((response) => {
-            const ImageUploadResponse = protobuf.roots.default.lookupType("main.ImageUploadResponse");
-            const res = ImageUploadResponse.decode(new Uint8Array(response.data));
-            return res
-        })
-        .catch((error) => {
-            console.log(error);
-        })
-    }
-
-    return { upload };
-}
+  return { upload };
+};
 
 export default useImageUploder;
